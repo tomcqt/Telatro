4f1275f5cdac0c3bb54553dc3f62d141bca580e0
tomcat with a q <hi@tomcat.sh>
2025-03-15 17:28:59 -0400

    Start to mod the loader and update build script.

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
-cd ..
 echo "Building..."
-npx love.js Telatro Telatro/bin -t Telatro -m 1149289489 -s ASSERTIONS=1 -c -v
+rm -rv src
+mkdir src
+cp -rv engine src
+cp -rv functions src
+cp -rv localization src
+cp -rv resources src
+rm -rv src/resources/textures_preresize
+cp -rv *.lua src
+cp -rv version.jkr src
+npx love.js src bin -t Telatro -m 1149289489 -s ASSERTIONS=1 -c -v
+cp -fvr mod/* bin
 echo "Built!"
-cd Telatro
